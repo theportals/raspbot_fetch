e4c32180f09078911183483e7674b493f9d2b5f1
the_portals <clitchedglock94@gmail.com>
2023-11-24 14:08:55 -0700

    Initial commit; detect ball in frame

---
+++ src/raspbot/raspbot/fetch.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@作者: 古月居(www.guyuehome.com)
+@说明: ROS2话题示例-订阅图像话题
+"""
+
+import rclpy                            # ROS2 Python接口库
+from rclpy.node import Node             # ROS2 节点类
+from sensor_msgs.msg import Image       # 图像消息类型
+from std_msgs.msg import Int16
+from cv_bridge import CvBridge          # ROS与OpenCV图像转换类
+import cv2                              # Opencv图像处理库
+import numpy as np                      # Python数值计算库
+import imutils
+
+lower_red = np.array([0, 90, 128])      # 红色的HSV阈值下限
+upper_red = np.array([180, 255, 255])   # 红色的HSV阈值上限
+orangeLower = (20, 40, 40)
+orangeUpper = (70, 255, 255)
+
+"""
+创建一个订阅者节点
+"""
+class ImageSubscriber(Node):
+    def __init__(self, name):
+        super().__init__(name)                                  # ROS2节点父类初始化
+        self.sub = self.create_subscription(
+            Image, 'rare', self.listener_callback, 10)     # 创建订阅者对象（消息类型、话题名、订阅者回调函数、队列长度）
+        self.cv_bridge = CvBridge()                             # 创建一个图像转换对象，用于OpenCV图像与ROS的图像消息的互相转换
+        self.publisher = self.create_publisher(Int16, 'foobar', 10)
+
+    def object_detect(self, image):
+        blurred = cv2.GaussianBlur(image, (11, 11), 0)
+        hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+
+        mask = cv2.inRange(hsv, orangeLower, orangeUpper)
+        mask = cv2.erode(mask, None, iterations=2)
+        mask = cv2.dilate(mask, None, iterations=2)
+
+        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+            cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        center = None
+
+        msg = Int16()
+        msg.data = -1
+
+        if len(cnts) > 0:
+            c = max(cnts, key=cv2.contourArea)
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+            M = cv2.moments(c)
+            # THIS IS WHERE WE FIND OUR CENTER. PUBLISH THIS LATER
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+            self.get_logger().info(f"x: {int(x)}, y: {int(y)}, r: {int(radius)}")
+
+            if radius > 10:
+                cv2.circle(image, (int(x), int(y)), int(radius),
+                    (0, 255, 255), 2)
+                cv2.circle(image, center, 5, (0, 0, 255), -1)
+                msg.data = int(x)
+        self.publisher.publish(msg)
+        cv2.imshow("Image", image)
+        key = cv2.waitKey(1) & 0xFF
+
+
+    def listener_callback(self, data):
+#        self.get_logger().info('Receiving video frame')         # 输出日志信息，提示已进入回调函数
+        image = self.cv_bridge.imgmsg_to_cv2(data, 'bgr8')      # 将ROS的图像消息转化成OpenCV图像
+        self.object_detect(image)                               # 苹果检
+
+
+def main(args=None):                                        # ROS2节点主入口main函数
+    rclpy.init(args=args)                                   # ROS2 Python接口初始化
+    node = ImageSubscriber("topic_webcam_sub")              # 创建ROS2节点对象并进行初始化
+    rclpy.spin(node)                                        # 循环等待ROS2退出
+    node.destroy_node()                                     # 销毁节点对象
+    rclpy.shutdown()                                        # 关闭ROS2 Python接口
